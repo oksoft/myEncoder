2349353c81ba650f1624c8be711e95b4a0228b60
Oregu <oregu@oregu.ru>
2015-06-28 18:39:56 +0300

    Библиотека управления энкодером. Комменты в части файлов набраны в кодировке cp1251

---
+++ myEncoder.cpp
@@ -0,0 +1,69 @@
+/*
+Библиотека для управления энкодером.
+Написана 24.06.2015.
+ 
+*/
+
+
+
+#include "Arduino.h"    // обязательно для любой библиотеки под ардуино
+#include "myEncoder.h"  // По правилам включаем заголовочный файл модуля в сам модуль
+
+
+CEncoder::CEncoder(int pin1, int pin2, int pin3){
+  _pin1 = pin1;
+  _pin2 = pin2;
+  _pin3 = pin3;
+  _mySec = 0;
+  _state = 0;
+  _dec = 0;
+  pinMode(_pin1, INPUT);
+  pinMode(_pin2, INPUT);
+  pinMode(_pin3, INPUT);
+  digitalWrite(_pin1, HIGH);
+  digitalWrite(_pin2, HIGH);
+  digitalWrite(_pin3, HIGH); 
+};  
+
+
+int CEncoder::check(){     // Возвращает 1 для поворота по часовой стрелке или -1 при повороте против часовой стрелки
+  if ((millis() - _mySec)<1) return(0);  // Ограничиваем опрос энкодера частотой 1кГц, чтобы не выполнять лишние команды в цикле
+  int result =0;
+  _mySec = millis();
+  int New = digitalRead(_pin1) + 2*digitalRead(_pin2);
+  switch (_state) {
+        case 2:            
+		{
+		if(New == 3) result = 1;
+		if(New == 0) result = -1;
+		break;
+		}
+ 	case 0:
+		{
+		if(New == 2) result = 1;
+		if(New == 1) result = -1;
+		break;
+		}
+	case 1:
+		{
+		if(New == 0) result = 1;
+		if(New == 3) result = -1;
+		break;
+		}
+	case 3:
+		{
+		if(New == 1) result = 1;
+		if(New == 2) result = -1;
+		break;
+		} 
+  }
+  _state = New;
+  if (result != 0) _dec++;
+  if (_dec==2) {result = 0;_dec = 0;};
+    
+  return(result);  
+};
+
+bool CEncoder::button_down(){   // Возвращает true? если кнопка нажата!!! т.е. низкий сигнал в порте!!! 
+  return(!digitalRead(_pin3));
+};
---
+++ myEncoder.h
@@ -0,0 +1,28 @@
+/*
+Библиотека для управления энкодером.
+Написана 23.06.2015.
+ 
+*/
+
+
+#ifndef __MYENCODER_H__ 
+#define __MYENCODER_H__
+
+
+
+class CEncoder
+{
+  public:
+    CEncoder(int pin1, int pin2, int pin3);
+    int check();
+    bool button_down();
+  private:
+    int _pin1;
+    int _pin2;
+    int _pin3;
+    long _mySec;
+    int _state; 
+    int _dec;
+};
+
+#endif //__MYENCODER_H__
